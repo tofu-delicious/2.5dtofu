e3bda5f89a3c4939e27229f0f5e968bbab2c962d
tofudelicious <1386175@st.kobedenshi.ac.jp>
2026-06-14 14:24:42 +0900

    6月14日 TitleSceneのボタンUIを仮表示
    StartボタンとSoundボタンを仮表示。

--- tofu/Src/Application/Scene/TitleScene/TitleScene.cpp
+++ tofu/Src/Application/Scene/TitleScene/TitleScene.cpp
@@ -29,8 +29,22 @@ void TitleScene::Init()
 
 	//タイル
 	m_tile = std::make_shared<C_UIBase>();
-	std::shared_ptr<KdTexture> tex = C_AssetManager::Instance().GetTex("Tile");
-	m_tile->Init(TILE_POS,TILE_RECT,tex);
+	std::shared_ptr<KdTexture> tileTex = C_AssetManager::Instance().GetTex("Tile");
+	m_tile->Init(TILE_POS,TILE_RECT,tileTex);
 	m_tile->SetCamera(m_spCamera->GetKdCamera());
 	AddObject(m_tile);
+
+	//スタートボタン
+	m_start = std::make_shared<C_UIBase>();
+	std::shared_ptr<KdTexture> startTex = C_AssetManager::Instance().GetTex("TitleButton");
+	m_start->Init(START_POS, START_RECT,startTex);
+	m_start->SetCamera(m_spCamera->GetKdCamera());
+	AddObject(m_start);
+
+	//音量設定ボタン
+	m_sound = std::make_shared<C_UIBase>();
+	std::shared_ptr<KdTexture> soundTex = C_AssetManager::Instance().GetTex("TitleButton");
+	m_sound->Init(SOUND_POS, SOUND_RECT,soundTex);
+	m_sound->SetCamera(m_spCamera->GetKdCamera());
+	AddObject(m_sound);
 }
--- tofu/Src/Application/Scene/TitleScene/TitleScene.h
+++ tofu/Src/Application/Scene/TitleScene/TitleScene.h
@@ -25,6 +25,14 @@ private :
 	static constexpr Math::Vector3 TILE_POS = { 0.0f,0.0f,0.0f };
 	static constexpr Math::Rectangle TILE_RECT = { 0,0,1280,720 };
 
+	static constexpr Math::Vector3 START_POS = { 0.0f,0.2f,0.0f };
+	static constexpr Math::Rectangle START_RECT = { 0,0,192,64 };
+
+	static constexpr Math::Vector3 SOUND_POS = { 0.0f,-0.2f,0.0f };
+	static constexpr Math::Rectangle SOUND_RECT = { 0,64,192,64 };
+
 	//======== インスタンス ==========
 	std::shared_ptr<C_UIBase> m_tile = nullptr;		//タイル（背景）
+	std::shared_ptr<C_UIBase> m_start = nullptr;	//スタートボタン
+	std::shared_ptr<C_UIBase> m_sound = nullptr;	//音量設定ボタン
 };
